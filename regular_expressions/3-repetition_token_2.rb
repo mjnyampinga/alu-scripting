4af2aee3b7a0943b747759c672984fba34193e9f
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-05-17 09:07:20 +0000

    task3

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -1,9 +1,2 @@
-input = ARGV[0]
-
-pattern = /a{3,5}/
-
-if input =~ pattern
-  puts "Match found: #{input}"
-else
-  puts "No match found"
-end
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/h[bt]+[bt]+n/).join
